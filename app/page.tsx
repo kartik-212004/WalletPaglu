ce8d02fe3dfa2f9ac3883a157dfb1ee9dea2b099
Kartik <kartik200421@gmail.com>
2025-04-14 12:38:25 +0530

    added hrefs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { motion } from "motion/react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <motion.div
-      initial={{ y: "-90%", opacity: 0 }}
+      initial={{ y: "-50%", opacity: 0 }}
       animate={{ y: "0%", opacity: 100 }}
       transition={{ duration: 0.5 }}
       className="text-primary"
@@ -14,8 +15,12 @@ export default function Home() {
         <div className="text-5xl  font-bold">Supported Blockchains</div>
         <div className="text-2xl dark:text-gray-400">Choose A Blockchain</div>
         <div className="flex flex-row space-x-3">
-          <Button className="text-sm font-mono">Enterium</Button>
-          <Button className="text-sm font-mono">Solana</Button>
+          <Link href={"/wallet"}>
+            <Button className="text-sm font-mono">Enterium</Button>
+          </Link>
+          <Link href={"/wallet"}>
+            <Button className="text-sm font-mono">Solana</Button>
+          </Link>
         </div>
       </div>
     </motion.div>
