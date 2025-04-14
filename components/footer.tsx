f0f17be5d391ca9d734e1c8c1e19d4f4444dae80
Kartik <kartik200421@gmail.com>
2025-04-14 13:00:46 +0530

    added footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 export default function Footer() {
   return (
-    <div className="border-t dark:border-[#2a3039] tracking-wide py-8 font-medium text-xl w-[55%] container mx-auto bottom-0 ">
+    <div className="border-t  dark:text-gray-400 dark:border-[#434952] tracking-wide py-8 font-semibold text-lg w-[55%] container mx-auto bottom-0 ">
       Developed By{" "}
-      <Link href={"https://github.com/kartik-212004"} className="font-bold">
+      <Link
+        href={"https://github.com/kartik-212004"}
+        className="font-bold dark:text-white"
+      >
         Kartik
       </Link>
     </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,16 @@
 import { ModeToggle } from "./theme";
 import { Bitcoin } from "lucide-react";
+import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="flex text-primary flex-row w-full mb-11 justify-around p-5">
-      <div className="font-bold text-3xl flex flex-row space-x-3 justify-center items-center">
+      <Link
+        href={"/"}
+        className="font-bold text-3xl flex flex-row space-x-3 justify-center items-center"
+      >
         <Bitcoin className="size-10" />
         <span> Laxmi Crypto Funds</span>
-      </div>
+      </Link>
       <div className="w-[4vw]">
         <ModeToggle />
       </div>
