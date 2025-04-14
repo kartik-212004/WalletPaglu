3488004f582c848fcef4bdeadb999391e7cd44a3
Kartik <kartik200421@gmail.com>
2025-04-14 13:14:43 +0530

    fixed bug in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,31 @@
+"use client";
 import Link from "next/link";
+import { motion } from "motion/react";
 export default function Footer() {
   return (
-    <div className="border-t  dark:text-gray-400 dark:border-[#434952] tracking-wide py-8 font-semibold text-lg w-[55%] container mx-auto bottom-0 ">
-      Developed By{" "}
-      <Link
-        href={"https://github.com/kartik-212004"}
-        className="font-bold dark:text-white"
-      >
-        Kartik
-      </Link>
-    </div>
+    <motion.div
+      initial={{ y: "50%", opacity: 0 }}
+      animate={{ y: "0%", opacity: 100 }}
+      transition={{ duration: 0.5 }}
+      className="border-t flex flex-row items-center dark:text-gray-400 space-x-2 dark:border-[#434952] tracking-wide font-semibold text-lg w-[55%] container mx-auto bottom-0 "
+    >
+      <span>
+        Developed By{" "}
+        <Link
+          href={"https://github.com/kartik-212004"}
+          className="font-bold dark:text-white"
+        >
+          Kartik
+        </Link>
+      </span>
+
+      <div>
+        <img
+          className="w-20"
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGhyMWdvYW1qYTh2OXlnbmY3cGl4ZGlyMTNjZmtndWQxMWhyNWRoNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/W3fOvEAyRZ9TxDsf3Y/giphy.gif"
+          alt=""
+        />
+      </div>
+    </motion.div>
   );
 }
